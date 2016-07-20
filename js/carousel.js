2ad4010aa13084fed98a2f44a1d18271ec765a1a
Hamilton Pytluk <hamiltonpytluk@gmail.com>
2016-07-20 17:27:12 -0500

    Initial commit

---
+++ js/carousel.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+  $('<a class="left-button" href="#"></a><a class="right-button" href="#"></a>').appendTo('#carousel');
+
+  $('#carousel li').each(function(){
+    $('<div class="darken"></div><img src="img/shine.png" class="shine" />').appendTo($(this));
+    $('<h3>'+$(this).find('img').attr('title')+'</h3><span>'+$(this).find('img').data('desc')+'</span>').appendTo($(this));
+  });
+
+  $('a.left-button').on('click', function(e){
+
+    e.preventDefault();
+
+    $('.left, .right').css('z-index','');
+    $('.left').removeClass('left').addClass('left-to-current');
+    $('.right').removeClass('right').addClass('right-to-left');
+    $('.current').removeClass('current').addClass('current-to-right');
+
+    setTimeout(function(){
+      $('.left-to-current').removeClass('left-to-current').addClass('current');
+      $('.right-to-left').removeClass('right-to-left').addClass('left');
+      $('.current-to-right').removeClass('current-to-right').addClass('right');
+    }, 500);
+
+  });
+
+  $('a.right-button').on('click', function(e){
+
+    e.preventDefault();
+
+    $('.left, .right').css('z-index','');
+    $('.left').removeClass('left').addClass('left-to-right').css('z-index','0');
+    $('.right').removeClass('right').addClass('right-to-current');
+    $('.current').removeClass('current').addClass('current-to-left').css('z-index','2');
+
+    setTimeout(function(){
+      $('.right-to-current').removeClass('right-to-current').addClass('current');
+      $('.left-to-right').removeClass('left-to-right').addClass('right');
+      $('.current-to-left').removeClass('current-to-left').addClass('left');
+    }, 500);
+
+  });
+
+});
